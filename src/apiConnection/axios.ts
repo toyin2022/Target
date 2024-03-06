61b796604142b11659e93fa18735f3a2e154e8dd
toyin2022 <104086364+toyin2022@users.noreply.github.com>
2024-03-06 17:24:54 +0100

    Update axios

--- src/apiConnection/axios.ts
+++ src/apiConnection/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://target-api-1.onrender.com",
   withCredentials: true,
 });
 export default axiosInstance;
